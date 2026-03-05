14f351dfda9bef9427150f7b7d76d4103bbf465a
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-05 09:53:14 -0300

    simplificando os códigos de saída

--- calculadoraDigital/src/CalculadoraTeste/TesteCalculadora.java
+++ calculadoraDigital/src/CalculadoraTeste/TesteCalculadora.java
@@ -3,6 +3,9 @@ package CalculadoraTeste;
 import static org.junit.Assert.*; 
 import org.junit.Test; 
 import FontesAppCalculadora.Calculadora; 
+import org.junit.After; 
+
+import org.junit.Before; 
 
 
 
@@ -10,6 +13,23 @@ public class TesteCalculadora {
 	//atributos
 	private static int contadorTeste = 0;
 	private String nomeTeste = "";
+	private double resultadoReal = 0;
+	
+	//preparacao teste
+	@Before
+	public void preparaTeste() {
+	   contadorTeste = contadorTeste + 1;
+       System.out.print("teste: ");
+       System.out.println(contadorTeste);
+	}
+	//finalizacao teste
+	@After
+	public void encerraTeste() {
+       System.out.println(nomeTeste);
+       System.out.print("resultado= ");
+       System.out.println(resultadoReal);
+	}
+	
 	/**
 	 * Teste de somar na Calculadora. 
 	 */ 
@@ -18,10 +38,6 @@ public class TesteCalculadora {
 
 	   public void testeSomar5com5() { 
 		   nomeTeste = "Somar 5 com 5";
-		   contadorTeste = contadorTeste + 1;
-	       System.out.print("teste: ");
-	       System.out.println(contadorTeste);
-
 
 		   double nro1 = 5; 
 	
@@ -31,12 +47,10 @@ public class TesteCalculadora {
 
 	       double resultadoEsperado = 10; 
 
-	       double resultadoReal= calc.somar(nro1, nro2);       
+	       resultadoReal= calc.somar(nro1, nro2);       
 
 	       assertEquals(resultadoEsperado, resultadoReal, 0); 
-	       System.out.println(nomeTeste);
-	       System.out.print("resultado= ");
-	       System.out.println(resultadoReal);
+
 	   } 
 
 	 
@@ -59,12 +73,10 @@ public class TesteCalculadora {
 
 	      double resultadoEsperado= 2; 
 
-	      double resultadoReal= calc.subtrair(nro1, nro2); 
+	      resultadoReal= calc.subtrair(nro1, nro2); 
 
 	      assertEquals(resultadoEsperado, resultadoReal, 0);
-	      System.out.println(nomeTeste);
-	      System.out.print("resultado= ");
-	      System.out.println(resultadoReal);
+
 
 	  } 
 
@@ -90,12 +102,10 @@ public class TesteCalculadora {
 
 	       double resultadoEsperado = 9; 
 
-	       double resultadoReal = calc.multiplicar(nro1, nro2); 
+	       resultadoReal = calc.multiplicar(nro1, nro2); 
 
 	       assertEquals(resultadoEsperado, resultadoReal, 0); 
-	       System.out.println(nomeTeste);
-	       System.out.print("resultado= ");
-	       System.out.println(resultadoReal);
+
 	   } 
 
 	 
@@ -120,12 +130,10 @@ public class TesteCalculadora {
 
 	      double resultadoEsperado= 1; 
 
-	      double resultadoReal = calc.dividir(nro1, nro2); 
+	      resultadoReal = calc.dividir(nro1, nro2); 
 
 	      assertEquals(resultadoEsperado, resultadoReal, 0.1); 
-	      System.out.println(nomeTeste);
-	      System.out.print("resultado= ");
-	      System.out.println(resultadoReal);
+;
 	  } 
 
 	  public void testeDividir3por7() { 
@@ -140,12 +148,10 @@ public class TesteCalculadora {
 
 	      double resultadoEsperado= 0.42857; 
 
-	      double resultadoReal = calc.dividir(nro1, nro2); 
+	      resultadoReal = calc.dividir(nro1, nro2); 
 
 	      assertEquals(resultadoEsperado, resultadoReal, 0.00001);
-	      System.out.println(nomeTeste);
-	      System.out.print("resultado= ");
-	      System.out.println(resultadoReal);
+
 
 	  } 
 	     
